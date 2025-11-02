da6fdbb3860c3e1f4c703ffcf666b3ac1fe5919b
Jose David <leonruizjosedavid44@gmail.com>
2025-11-02 15:59:29 -0500

    add test 3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, adapter);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+  await app.init();
 }
+
 bootstrap();
+
+export default expressApp;
